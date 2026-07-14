d4655a79380d7e0eac90717e7008fd95bde2f189
gregallman007-web <greg.allman007@gmail.com>
2026-07-14 16:03:37 +0100

    Add files via upload

---
+++ brand/Group1000001936.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface Group1000001936Props {
+  className?: string;
+  style?: React.CSSProperties;
+}
+export declare const Group1000001936: React.FC<Group1000001936Props>;
+export default Group1000001936;
